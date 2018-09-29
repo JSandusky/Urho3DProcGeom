25b0dbfa63d6bd8e4d790042c160999c0efdac95
JSandusky <j.sandusky@hotmail.com>
2018-09-29 15:01:51 -0400

    Add UV tests for charting and planar

--- Tests/Test_UVCoords.cpp
+++ Tests/Test_UVCoords.cpp
@@ -1,12 +1,30 @@
-#include <Urho3D/ProcGeom/TexCoords.h>
+#include <Urho3D/IO/File.h>
+#include <Urho3D/Graphics/Geometry.h>
+#include <Urho3D/Graphics/Model.h>
 #include <Urho3D/Resource/ResourceCache.h>
+#include <Urho3D/ProcGeom/TexCoords.h>
 
 namespace Urho3D
 {
 
 void Test_DXUVAtlas(Context* ctx)
 {
-    
+    auto mdl = ctx->GetSubsystem<ResourceCache>()->GetResource<Model>("Models/Cylinder.mdl");
+    auto geo = mdl->GetGeometry(0, 0);
+    auto newGeo = CalculateUV(geo, 512, 512, 2, 10, 0.25f, 4.0, 0);
+    auto outFile = SharedPtr<File>(new File(ctx, "Test_DXUVAtlas.obj", Urho3D::FILE_WRITE));
+    WriteGeometryToOBJ({ newGeo }, outFile, false, true, false);
+    delete mdl;
+}
+
+void Test_PlanarUV(Context* ctx)
+{
+    auto mdl = ctx->GetSubsystem<ResourceCache>()->GetResource<Model>("Models/Cylinder.mdl");
+    auto geo = mdl->GetGeometry(0, 0);
+    auto newGeo = PlanarUV(geo, Urho3D::Plane(Vector3::FORWARD, Vector3::ZERO);
+    auto outFile = SharedPtr<File>(new File(ctx, "Test_PlanarUV.obj", Urho3D::FILE_WRITE));
+    WriteGeometryToOBJ({ newGeo }, outFile, false, true, false);
+    delete mdl;
 }
 
 }
